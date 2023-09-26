0e3f6004508aa0fd8dc78a11a3f66724f046b619
rettichlp <rettichlp01@gmail.com>
2023-09-26 18:02:40 +0200

    Update redirect handling

--- src/main/java/com/rettichlp/redirect/RedirectInterceptor.java
+++ src/main/java/com/rettichlp/redirect/RedirectInterceptor.java
@@ -1,6 +1,9 @@
 package com.rettichlp.redirect;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
@@ -20,11 +23,19 @@ public class RedirectInterceptor implements HandlerInterceptor {
         String queryParameters = Optional.ofNullable(request.getQueryString()).orElse("");
 
         String redirect = "https://rettichlp.de:8443" + uri + (!queryParameters.isBlank() ? "?" + queryParameters : "");
-        response.sendRedirect(redirect);
 
-        ServerApplication.LOGGER.info("Redirect {} with parameters {}", uri, queryParameters.replace("&", " and "));
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<String> responseEntity = restTemplate.getForEntity(redirect, String.class);
+
+        HttpStatus statusCode = responseEntity.getStatusCode();
+        String responseBody = Optional.ofNullable(responseEntity.getBody()).orElse("");
+
+        ServerApplication.LOGGER.info("Redirect set: {} {} [{}]", uri, "with parameters " + queryParameters.replace("&", " and "), statusCode);
+
+        response.setStatus(statusCode.value());
+        response.getWriter().write(responseBody);
+        response.getWriter().flush();
 
         return false;
     }
 }
-
