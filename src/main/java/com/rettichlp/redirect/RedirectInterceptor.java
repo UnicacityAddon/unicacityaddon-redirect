c4caf0c2a8ad9ba8f21edfbd361472e208227d11
rettichlp <rettichlp01@gmail.com>
2023-09-28 16:54:28 +0200

    Fix url encoding

--- src/main/java/com/rettichlp/redirect/RedirectInterceptor.java
+++ src/main/java/com/rettichlp/redirect/RedirectInterceptor.java
@@ -4,11 +4,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpServerErrorException;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
 import java.util.Optional;
 
 @Component
@@ -16,12 +19,13 @@ public class RedirectInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        String uri = request.getRequestURI();
+        String uri = URLDecoder.decode(request.getRequestURI(), StandardCharsets.UTF_8);
+        System.out.println(uri);
         if (!uri.contains("unicacityaddon/v1/")) {
             return true;
         }
 
-        String queryParameters = Optional.ofNullable(request.getQueryString()).orElse("");
+        String queryParameters = Optional.ofNullable(request.getQueryString()).map(s -> URLDecoder.decode(s, StandardCharsets.UTF_8)).orElse("");
         String redirect = "https://rettichlp.de:8443" + uri + (!queryParameters.isBlank() ? "?" + queryParameters : "");
 
         HttpStatus statusCode;
@@ -32,12 +36,14 @@ public class RedirectInterceptor implements HandlerInterceptor {
             ResponseEntity<String> responseEntity = restTemplate.getForEntity(redirect, String.class);
             statusCode = responseEntity.getStatusCode();
             responseBody = Optional.ofNullable(responseEntity.getBody()).orElse("");
-        } catch (HttpClientErrorException e) {
+        } catch (HttpClientErrorException | HttpServerErrorException e) {
             statusCode = e.getStatusCode();
             responseBody = e.getResponseBodyAsString();
         }
 
-        ServerApplication.LOGGER.info("Redirect set: {}{} [{}]", uri, queryParameters.replace("&", " and "), statusCode);
+        if (!uri.contains("/authorize")) {
+            ServerApplication.LOGGER.info("Redirect set: {}{}{} [{}]", uri, !queryParameters.isBlank() ? "?" : "", queryParameters.replace("&", " and "), statusCode);
+        }
 
         response.setStatus(statusCode.value());
         response.getWriter().write(responseBody);
